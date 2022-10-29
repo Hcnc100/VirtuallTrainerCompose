d10171f0a598948ad2ad0de22575136e88506e83
Death <ricardo.pajaro.bc2d@gmail.com>
2022-10-29 13:01:39 -0500

    refactor ExerciseScreen.kt

--- app/src/main/java/com/d34th/nullpointer/virtualtrainercompose/ui/screens/exercise/ExerciseScreen.kt
+++ app/src/main/java/com/d34th/nullpointer/virtualtrainercompose/ui/screens/exercise/ExerciseScreen.kt
@@ -7,8 +7,11 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.d34th.nullpointer.virtualtrainercompose.R
 import com.d34th.nullpointer.virtualtrainercompose.core.utils.shareViewModel
+import com.d34th.nullpointer.virtualtrainercompose.models.Exercise
 import com.d34th.nullpointer.virtualtrainercompose.presentation.ExerciseViewModel
 import com.d34th.nullpointer.virtualtrainercompose.ui.screens.destinations.CameraScreenDestination
 import com.d34th.nullpointer.virtualtrainercompose.ui.screens.destinations.DataUserScreenDestination
@@ -21,26 +24,40 @@ import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 @Destination(start = true)
 @Composable
 fun ExercisesScreen(
-    exerciseViewModel: ExerciseViewModel = shareViewModel(),
-    navigator: DestinationsNavigator
+    navigator: DestinationsNavigator,
+    exerciseViewModel: ExerciseViewModel = shareViewModel()
 ) {
     Scaffold(topBar = {
         ToolbarSettings(
-            title = "Lista de ejercicios",
+            title = stringResource(id = R.string.app_name),
             actionSettings = { navigator.navigate(DataUserScreenDestination) })
-    }) {
-        LazyVerticalGrid(
-            columns = GridCells.Adaptive(250.dp),
-            modifier = Modifier.padding(it),
-        ) {
-            items(exerciseViewModel.listExercise) { exercise ->
-                ExerciseItem(
-                    exercise = exercise,
-                    onClickExercise = {
-                        exerciseViewModel.loadModel(exercise.nameModel)
-                        navigator.navigate(CameraScreenDestination)
-                    })
+    }) { paddingValues ->
+        ListExercises(
+            modifier = Modifier.padding(paddingValues),
+            listExercise = exerciseViewModel.listExercise,
+            actionClickModel = {
+                exerciseViewModel.loadModel(it.nameModel)
+                navigator.navigate(CameraScreenDestination)
             }
+        )
+    }
+}
+
+@Composable
+private fun ListExercises(
+    listExercise: List<Exercise>,
+    modifier: Modifier = Modifier,
+    actionClickModel: (Exercise) -> Unit
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(250.dp),
+        modifier = modifier,
+    ) {
+        items(listExercise, key = { it.nameModel }) { exercise ->
+            ExerciseItem(
+                exercise = exercise,
+                onClickExercise = { actionClickModel(exercise) }
+            )
         }
     }
 }
